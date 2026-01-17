39c5d0e500b44478183c98d8ff96f9ad4dc873a7
HTTYDKing <s2335450@ed.ac.uk>
2026-01-17 23:34:24 +0400

    Software Testing

--- src/test/java/com/PizzaDrone/ILPWebService/Unit_testing/PosDistanceTest.java
+++ src/test/java/com/PizzaDrone/ILPWebService/Unit_testing/PosDistanceTest.java
@@ -11,22 +11,22 @@ import java.io.InputStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PosDistanceTest {
-    @Test
-    void TestValidPosDistance() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-
-        try (InputStream is = getClass()
-                .getClassLoader()
-                .getResourceAsStream("ValidateOrder/valid_order.json")) {
-
-            if (is == null) {
-                throw new IllegalStateException("Test JSON file not found");
-            }
-
-            PizzaOrder order = mapper.readValue(is, PizzaOrder.class);
-            ValidateOrder validateOrder = new ValidateOrder(order);
-
-            assertEquals(OrderStatus.VALID, validateOrder.getStatus());
-        }
-    }
+//    @Test
+//    void TestValidPosDistance() throws Exception {
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        try (InputStream is = getClass()
+//                .getClassLoader()
+//                .getResourceAsStream("ValidateOrder/valid_order.json")) {
+//
+//            if (is == null) {
+//                throw new IllegalStateException("Test JSON file not found");
+//            }
+//
+//            PizzaOrder order = mapper.readValue(is, PizzaOrder.class);
+//            ValidateOrder validateOrder = new ValidateOrder(order);
+//
+//            assertEquals(OrderStatus.VALID, validateOrder.getStatus());
+//        }
+//    }
 }
